51695af493c464c9bf8f80255ec506f49c01379b
SuriSureda <suredaandreu@gmail.com>
2021-02-14 11:50:02 +0100

    getJWT ( token generation )

--- src/Auth.ts
+++ src/Auth.ts
@@ -1,6 +1,9 @@
-import {hashSync, compareSync} from 'bcryptjs';
+import IAuthUser from './models/IAuthUser';
+import {sign} from 'jsonwebtoken';
+import {compareSync, hashSync} from 'bcryptjs'
 
 export default class Auth{
+
 
     private static  salt_number : number = 10;
 
@@ -10,5 +13,9 @@ export default class Auth{
 
     static comparePassword(password : string, encrypted : string) : boolean{
         return compareSync(password, encrypted);
+    }
+
+    static getJWT(payload : IAuthUser) : string{
+        return sign(payload, process.env.JWT_SECRET_KEY, {expiresIn : "1d"});
     }
 }
