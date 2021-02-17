8894bc3d9bace6b41851c2969d0a29d962630732
SuriSureda <suredaandreu@gmail.com>
2021-02-17 19:19:22 +0100

    wake route added ( CommonRoute )

--- src/Routes/CommonRoute.ts
+++ src/Routes/CommonRoute.ts
@@ -7,6 +7,11 @@ export default class CommonRoute{
     private app : Application;
 
     public route(app : Application) : void{
+        //route to avoid heroku to fall aslepp
+        app.get('/wake', (req : Request, res : Response) => {
+            CustomResponse.successResponse("not asleep yet",null,res);
+        })
+
         app.all('*', (req: Request, res: Response)=> {
             CustomResponse.badRequest("Check URL", res);
         })
